558997162f2afb8923ab8b406ca3eb33e71fd43b
Kishan Sheth <kishansheth21@gmail.com>
2021-02-24 19:30:03 +0530

    as

--- DBConnection.js
+++ DBConnection.js
@@ -1,4 +1,7 @@
 const { Client } = require("@elastic/elasticsearch");
-const client = new Client({ node: "http://localhost:9200" });
+const client = new Client({
+  node:
+    "https://dpll67jqs4:fj1u60rf41@react-node-rapidops-9509018390.us-east-1.bonsaisearch.net:443",
+});
 
 module.exports = client;
